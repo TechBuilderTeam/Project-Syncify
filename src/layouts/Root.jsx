a6ae85106690240b4d092dd74ec34e67d91b321e
TechBuilderTeam <saashid18@gmail.com>
2024-05-10 20:36:51 +0600

    navbar with theme changer has been completed

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,11 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../pages/shared/Navbar";
+import { useState } from "react";
 
 
 const Root = () => {
+    const [isDarkTheme, setIsDarkTheme] = useState(false);
+    const toggleTheme = () => {
+        setIsDarkTheme((prevTheme) => !prevTheme);
+    };
     return (
-        <div>
-            <Navbar />
+        <div data-theme={isDarkTheme ? "night" : "light"}>
+            <Navbar  toggleTheme={toggleTheme}/>
            <Outlet />
         </div>
     );
