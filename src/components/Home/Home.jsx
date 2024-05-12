c70bf841368af5a2b978176728c566515ce429f6
Md Akther Hosen <dev.aktherhosen@gmail.com>
2024-05-12 11:59:40 +0600

    features card started

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,16 +2,15 @@ import Sponsors from "./Sponsors";
 import Banner from "./banner";
 import TopCompanySlider from "../Pages/Home/TopCampanySliderSection/TopCompanySlider";
 import DifferentTypeOfTabSection from "../Pages/Home/DifferentTypeOfTabSection/DifferentTypeOfTabSection";
-
-import Login from "../Pages/Login/login";
+import Login from "../Pages/Login";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <Sponsors />
-      <DifferentTypeOfTabSection/>
-      <TopCompanySlider/>
+      <DifferentTypeOfTabSection />
+      <TopCompanySlider />
       <Login />
     </div>
   );
---
+++ src/components/Pages/Home/FeatureSection/Features.jsx
@@ -0,0 +1,9 @@
+const Features = () => {
+  return (
+    <div>
+      <h1>This is feature section</h1>
+    </div>
+  );
+};
+
+export default Features;
