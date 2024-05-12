e4293be13488ac02639f70d906289f6b6dd55cc0
TechBuilderTeam <saashid18@gmail.com>
2024-05-12 16:39:06 +0600

    footer done with responsiveness

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./layouts/Root";
 import Home from "./components/Home/Home";
+import Login from "./components/Pages/Login/Login";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home />,
       },
+      {
+        path: "/login",
+        element: <Login />,
+      }
     ],
   },
 ]);
