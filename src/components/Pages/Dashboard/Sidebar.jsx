b67ebc52456b95b552e6eb99721d406774756934
TechBuilderTeam <saashid18@gmail.com>
2024-05-16 21:33:59 +0600

    some changes in the dashbooard

--- src/components/Pages/Dashboard/Sidebar.jsx
+++ src/components/Pages/Dashboard/Sidebar.jsx
@@ -1,47 +1,48 @@
-import React from 'react';
+
 import Dashboard from './Dashboard';
-import {FaBoxTissue,FaChalkboard, FaPoll} from 'react-icons/fa';
+import {FaBoxTissue,FaChalkboard} from 'react-icons/fa';
 import { CiViewTimeline } from "react-icons/ci";
 import { SlCalender } from "react-icons/sl";
 import { FaListUl } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
 
 const Sidebar = ({sidebarToggle}) => {
     return (
-        <div className={`${sidebarToggle ? "hidden" : "block"} w-30 md:w-60 bg-gradient-to-b from-[#73e9fe] to-[#78118f] fixed h-full px-4 py-2`}>
+        <div className={`${sidebarToggle ? "hidden" : "block"} w-30 md:w-60 bg-gradient-to-b from-[#73e9fe] dark:from-[#8401A1] to-[#78118f] dark:to-[#73e9fe] fixed h-full px-4 py-2`}>
             <div className='my-2 mb-4'>
-                <h1 className='text-2x text-white font-bold text-center'>Admin Dashboard</h1>
+                <h1 className='text-2x  font-bold text-center'>Admin Dashboard</h1>
             </div>
             <hr />
-            <ul className='mt-3 text-white font-bold'>
-                <li className='mb-2 rounded hover:shadow hover:bg-blue-500 py-2'>
-                    <a href="" className='px-3'>
+            <ul className='mt-3  font-bold'>
+                <li className='mb-2 rounded hover:shadow  py-2'>
+                    <Link to="" className='px-3'>
                        <CiViewTimeline className='inline-block w-6 h-6 mr-2 -mt-2'/>
                        Timeline
-                    </a>
+                    </Link>
                 </li>
-                <li className='mb-2 rounded hover:shadow hover:bg-blue-500 py-2'>
-                    <a href="" className='px-3'>
+                <li className='mb-2 rounded hover:shadow  py-2'>
+                    <Link to="" className='px-3'>
                        <FaChalkboard className='inline-block w-6 h-6 mr-2 -mt-2'/>
                        Board
-                    </a>
+                    </Link>
                 </li>
-                <li className='mb-2 rounded hover:shadow hover:bg-blue-500 py-2'>
-                    <a href="" className='px-3'>
+                <li className='mb-2 rounded hover:shadow  py-2'>
+                    <Link to="" className='px-3'>
                        <SlCalender className='inline-block w-6 h-6 mr-2 -mt-2'/>
-                       Calendar <span>NEW</span>
-                    </a>
+                       Calendar 
+                    </Link>
                 </li>
-                <li className='mb-2 rounded hover:shadow hover:bg-blue-500 py-2'>
-                    <a href="" className='px-3'>
+                <li className='mb-2 rounded hover:shadow  py-2'>
+                    <Link to="" className='px-3'>
                        <FaListUl className='inline-block w-6 h-6 mr-2 -mt-2'/>
                        List
-                    </a>
+                    </Link>
                 </li>
-                <li className='mb-2 rounded hover:shadow hover:bg-blue-500 py-2'>
-                    <a href="" className='px-3'>
+                <li className='mb-2 rounded hover:shadow  py-2'>
+                    <Link to="" className='px-3'>
                        <FaBoxTissue className='inline-block w-6 h-6 mr-2 -mt-2'/>
                        Isuues
-                    </a>
+                    </Link>
                 </li>
             </ul>
         </div>
