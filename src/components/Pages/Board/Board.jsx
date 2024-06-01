bf4b215f0048c8bba41c654c15f1e2b432413ba1
TechBuilderTeam <saashid18@gmail.com>
2024-06-02 00:46:55 +0600

    board redesign done

--- src/components/Pages/Board/Board.jsx
+++ src/components/Pages/Board/Board.jsx
@@ -5,12 +5,13 @@ import DragNDrop from "../Tasks/DragNDrop";
 import EditDeleteTask from "../Tasks/EditDeleteTask";
 import { useLocation } from "react-router-dom";
 import CreateBoard from "./CreateBoard";
+import NewBoard from "./NewBoard/NewBoard";
 
 const Board = () => {
     const location = useLocation();
 
     const timelineData = location.state;
-    console.log('timeline data recive in board from location state -> ',timelineData);
+    console.log('timeline data recive in board from location state -> ', timelineData);
     const { user } = useContext(AuthContext);
     const [tasks, setTasks] = useState([]);
     const [currentTask, setCurrentTask] = useState(null);
@@ -34,17 +35,34 @@ const Board = () => {
             })
             .catch(error => console.log(error));
     };
-    
+
     return (
         <div className="h-screen">
-            {/** timeline all data exist in timelineData variable */}
-            <div>
-                <CreateBoard timelineData={timelineData} />
+
+            <div className=" py-2">
+                <div className="flex justify-between items-center pb-2">
+                    <h1 className="text-3xl   pb-2 font-semibold ">
+                        Board
+                    </h1>
+                    
+                </div>
+
+                <hr className="w-full h-1 bg-gradient-to-r from-[#2c01a1] to-[#73e9fe] " />
+                <p className="text-sm  font-semibold mt-2 text-black dark:text-white  mb-10">
+                    To ensure seamless progress tracking and maintenance of your project, check out the boards into your project structure. Assign task and distinct roles to each member to streamline collaboration and enhance accountability throughout the project lifecycle. Also chat with your team members.
+                </p>
             </div>
-            <div>Timeline Details: {timelineData?.name} </div>
-            <CreateTask updateTasks={updateTasks} />
-          
-        </div>
+           
+        
+            {/** timeline all data exist in timelineData variable */ }
+    {/* <div>
+                <CreateBoard timelineData={timelineData} />
+            </div> */}
+    {/* <div>Timeline Details: {timelineData?.name} </div>
+            <CreateTask updateTasks={updateTasks} /> */}
+
+    <NewBoard />
+        </div >
     );
 };
 
