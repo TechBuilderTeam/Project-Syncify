2348c38f15cdb9fc5ef923aed5b730ba8ff529a4
Md Akther Hosen <dev.aktherhosen@gmail.com>
2024-06-03 13:12:05 +0600

    update footer, sponsor, slider cards text and title font-size

--- src/components/Pages/Home/HomeEmailSection/HomeEmailSection.jsx
+++ src/components/Pages/Home/HomeEmailSection/HomeEmailSection.jsx
@@ -4,7 +4,7 @@ import Animate from "../../../../../public/emailanimate.json";
 import { Link } from "react-router-dom";
 const HomeEmailSection = () => {
   return (
-    <div className="w-full h-fit px-4 md:px-40 py-4 my-4">
+    <div className="w-full h-fit px-4 md:px-40 py-4  mb-10 my-4">
       <div className="bg-gradient-to-b from-[#9d11bd] to-[#73e9fe] md:bg-gradient-to-r md:from-[#73e9fe] md:to-[#8401A1] dark:md:from-[#8401A1] dark:md:to-[#73e9fe] w-full rounded-2xl dark:text-[#8401A1] dark:md:text-[#73e9fe]">
         <div className="flex flex-col-reverse md:flex-row justify-center items-center pb-15 md:pb-10">
           <div className="w-full md:w-1/2 flex flex-col justify-center items-start md:pl-20 pl-10 md:px-0">
