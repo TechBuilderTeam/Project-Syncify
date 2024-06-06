d5c316571752b53cc4e991cbf6512eea71ea7aea
TechBuilderTeam <saashid18@gmail.com>
2024-06-06 20:38:39 +0600

    workspace delete issues fixed

--- src/components/Pages/Workspace/UserWorkspace.jsx
+++ src/components/Pages/Workspace/UserWorkspace.jsx
@@ -1,10 +1,12 @@
 import Workspace from "./Workspace";
 import CreateWorkspace from "./CreateWorkspace";
+import { useState } from "react";
 const UserWorkspace = () => {
+  const [load, setLoad] = useState(false);
   return (
     <div className="">
-      <CreateWorkspace className="" />
-      <Workspace />
+      <CreateWorkspace className="" load={load} setLoad={setLoad} />
+      <Workspace load={load} setLoad={setLoad}/>
     </div>
   );
 };
