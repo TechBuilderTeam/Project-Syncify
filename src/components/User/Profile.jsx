06e35d96b9f2a12863a47103d8ade6342880ff8e
Md Akther Hosen <dev.aktherhosen@gmail.com>
2024-06-06 11:40:29 +0600

    profile bug fixed

--- src/components/User/Profile.jsx
+++ src/components/User/Profile.jsx
@@ -21,18 +21,20 @@ const Profile = () => {
 
   const handleCloseModal = () => {
     document.getElementById("UserEdit").close();
-  }
+  };
 
   const handleAddUserEdit = () => {
     handleCloseModal();
     setEdit(true);
-  }
+  };
   useEffect(() => {
     if (user && user.userId) {
-      fetch(`https://projectsyncifyapi.onrender.com/api/v1/user/details/${user?.userId}/`)
+      fetch(
+        `https://projectsyncifyapi.onrender.com/api/v1/user/details/${user?.userId}/`
+      )
         .then((res) => {
           if (!res.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error("Network response was not ok");
           }
           return res.json();
         })
@@ -41,29 +43,31 @@ const Profile = () => {
           setProfile(data);
         })
         .catch((error) => {
-          console.error('Error fetching profile:', error);
+          console.error("Error fetching profile:", error);
         });
     }
   }, [user]);
 
-
-
-
-
   console.log({ profile });
   return (
     <div className=" py-10">
-
       {/* profile banner */}
       <div className="py-16 md:py-28 mb-40 bg-gradient-to-r from-sky-50 dark:from-sky-900 to-sky-500 dark:to-black relative ">
         <div className="flex flex-col md:flex-row justify-between items-center gap-10">
           <div className="w-full md:w-1/2">
             <div className="flex items-center gap-6">
               <div className="pl-10 md:pl-40">
-                <img src={user && profile?.image} alt={user && user.name} className="w-28 md:w-40 h-28 md:h-40 rounded-full border-4 border-sky-900 dark:border-sky-300" />
+                <img
+                  src={user && profile?.image}
+                  alt={user && user.name}
+                  className="w-28 md:w-40 h-28 md:h-40 rounded-full border-4 border-sky-900 dark:border-sky-300"
+                />
               </div>
               <div className="flex flex-col justify-start">
-                <h1 className="text-xl md:text-3xl font-bold"> {user && user.name.toUpperCase()}</h1>
+                <h1 className="text-xl md:text-3xl font-bold">
+                  {" "}
+                  {user && user.name.toUpperCase()}
+                </h1>
                 {/* <p className=""> {user && user.email}</p> */}
               </div>
             </div>
@@ -101,10 +105,9 @@ const Profile = () => {
             <div className="flex flex-wrap gap-2 sm:items-center mt-10 md:mt-6 ml-10 md:ml-36 font-semibold ">
               <button
                 onClick={() => setShowComponent("Info")}
-                className={`rounded py-1 px-2  text-sm hover:bg-sky-200 dark:hover:bg-sky-300 hover:text-sky-900 ${showComponent === "Info"
-                  ? "bg-sky-50 dark:bg-sky-950 "
-                  : ""
-                  }`}
+                className={`rounded py-1 px-2  text-sm hover:bg-sky-200 dark:hover:bg-sky-300 hover:text-sky-900 ${
+                  showComponent === "Info" ? "bg-sky-50 dark:bg-sky-950 " : ""
+                }`}
               >
                 Info
               </button>
@@ -128,68 +131,91 @@ const Profile = () => {
               </button> */}
               <button
                 onClick={() => setShowComponent("Contact")}
-                className={`rounded py-1 px-2  text-sm hover:bg-sky-200 dark:hover:bg-sky-300 hover:text-sky-900  ${showComponent === "Contact"
-                  ? "bg-sky-50 dark:bg-sky-950"
-                  : ""
-                  }`}
+                className={`rounded py-1 px-2  text-sm hover:bg-sky-200 dark:hover:bg-sky-300 hover:text-sky-900  ${
+                  showComponent === "Contact" ? "bg-sky-50 dark:bg-sky-950" : ""
+                }`}
               >
                 Contact
               </button>
-
             </div>
-
           </div>
           <div className="flex items-center justify-end text-4xl w-full md:w-1/3 mt-0 md:mt-28 mr-10 md:mr-40 mb-10">
-
             <div title="Edit">
-
-              <button onClick={() => document.getElementById('UserEdit').showModal()}><CiEdit className=""/></button>
-              <dialog id="UserEdit" className="modal modal-bottom sm:modal-middle">
+              <button
+                onClick={() => document.getElementById("UserEdit").showModal()}
+              >
+                <CiEdit className="" />
+              </button>
+              <dialog
+                id="UserEdit"
+                className="modal modal-bottom sm:modal-middle"
+              >
                 <div className="modal-box bg-white dark:bg-black">
-                  <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" onClick={() => document.getElementById("UserEdit").close()}>✕</button>
-                  <h3 className="font-bold text-2xl text-center dark:text-[#73e9fe] text-[#0c01a1] ">Edit</h3>
+                  <button
+                    className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+                    onClick={() => document.getElementById("UserEdit").close()}
+                  >
+                    ✕
+                  </button>
+                  <h3 className="font-bold text-2xl text-center dark:text-[#73e9fe] text-[#0c01a1] ">
+                    Edit
+                  </h3>
                   <form onSubmit={handleAddUserEdit}>
-
                     <div className="form-control">
                       <label className="label">
-                        <span className="label-text dark:text-[#73e9fe] text-[#0c01a1]">Name</span>
+                        <span className="label-text dark:text-[#73e9fe] text-[#0c01a1]">
+                          Name
+                        </span>
                       </label>
-                      <input type="text" name="name" className="input input-bordered bg-slate-200 dark:bg-black" required />
+                      <input
+                        type="text"
+                        name="name"
+                        className="input input-bordered bg-slate-200 dark:bg-black"
+                        required
+                      />
                     </div>
 
                     <div className="form-control">
                       <label className="label">
-                        <span className="label-text dark:text-[#73e9fe] text-[#0c01a1]">Position</span>
+                        <span className="label-text dark:text-[#73e9fe] text-[#0c01a1]">
+                          Position
+                        </span>
                       </label>
-                      <input type="text" name="position" className="input input-bordered bg-slate-200 dark:bg-black" required />
+                      <input
+                        type="text"
+                        name="position"
+                        className="input input-bordered bg-slate-200 dark:bg-black"
+                        required
+                      />
                     </div>
                     <div className="modal-action">
-                      <button type="submit" className="px-4 py-2 bg-gradient-to-r from-cyan-500 to-[#0c01a1] text-white  font-bold rounded w-full text-lg">Update</button>
+                      <button
+                        type="submit"
+                        className="px-4 py-2 bg-gradient-to-r from-cyan-500 to-[#0c01a1] text-white  font-bold rounded w-full text-lg"
+                      >
+                        Update
+                      </button>
                     </div>
                   </form>
                 </div>
               </dialog>
             </div>
-
-        
-            
           </div>
         </div>
       </div>
-      <div className=" absolute bg-gradient-to-r from-sky-200 dark:from-sky-700  to-sky-600 dark:to-black text-[#0c01a1] dark:text-white   -bottom-4 md:bottom-0  w-[80%]  left-1/2 -translate-x-1/2 translate-y-20 rounded">
+      {/* info  */}
+      <div className="translate-y-[-250px] bg-gradient-to-r from-sky-200 dark:from-sky-700  to-sky-600 dark:to-black text-[#0c01a1] dark:text-white -bottom-4 md:bottom-0 rounded ">
         {showComponent === "Info" ? (
           <Info user={user} profile={profile} />
-          // ) : showComponent === "Work" ? (
-          //   <Work />
-          // ) : showComponent === "Education" ? (
-          //   <Education />
-        ) : showComponent === "Contact" ? (
+        ) : // ) : showComponent === "Work" ? (
+        //   <Work />
+        // ) : showComponent === "Education" ? (
+        //   <Education />
+        showComponent === "Contact" ? (
           <UserContact />
-        )
-          : null}
+        ) : null}
       </div>
       {/* profile banner end */}
-
 
       {/* add the chatting component here */}
       <div className="fixed bottom-10 right-4">
@@ -198,7 +224,6 @@ const Profile = () => {
 
       {/* about section start */}
       <UserAbout />
-
 
       {/* portfolio section start */}
       <UserPortfolio />
@@ -213,15 +238,9 @@ const Profile = () => {
       {/* work section start */}
       <Work />
 
-
       {/* project in slider start */}
 
       <ProjectSlider user={user} profile={profile} />
-
-
-
-
-
     </div>
   );
 };
